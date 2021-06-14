798b62ab302eb85241a879aabfab966b735a9670
PedroG47 <49578117+PedroG47@users.noreply.github.com>
2021-06-14 15:09:28 -0300

    Delete Pratica_Flutter/lib/Node directory

--- Pratica_Flutter/lib/Node/Prática 4/1 -Soma/soma.js
+++
@@ -1,7 +0,0 @@
-function sum(){
-    var numero1 = document.querySelector(".numero1").value;
-    var numero2 = document.querySelector(".numero2").value;
-
-    var resultado = parseInt(numero1) + parseInt(numero2);
-    document.querySelector(".resultado").innerHTML = resultado;
-}
